8b8bc52b0ca1bdb14f24e04ce3b7bd0cf1bcf020
mr-l0n3lly <andreipatan@live.com>
2021-11-22 01:35:23 +0200

    Installed arch linux and configured a new env

---
+++ .config/fish/config.fish
@@ -0,0 +1,4 @@
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+config config status.showUntrackedFiles no
+
+neofetch
---
+++ bin/launch-rofi.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rofi -combi-modi window,drun,ssh -show combi -icon-theme "Papirus" -show-icons
